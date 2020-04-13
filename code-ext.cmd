243561381438d6cc0af522b607eb46fe77cbf4ab
Andrew Webster <webstean@gmail.com>
2020-04-14 00:36:55 +1000

    extensions

---
+++ code-ext.cmd
@@ -0,0 +1,31 @@
+@echo off
+@rem code --extensions-dir <dir>
+@rem    Set the root path for extensions.
+@rem code --list-extensions
+@rem     List the installed extensions.
+@rem code --show-versions
+@rem     Show versions of installed extensions, when using --list-extension.
+@rem code --install-extension (<extension-id> | <extension-vsix-path>)
+@rem     Installs an extension.
+@rem code --uninstall-extension (<extension-id> | <extension-vsix-path>)
+@rem     Uninstalls an extension.
+@rem code --enable-proposed-api (<extension-id>)
+@rem     Enables proposed API features for extensions. Can receive one or more extension IDs to enable individually.
+@rem 
+@rem 
+code --install-extension alexcvzz.vscode-sqlite
+code --install-extension austin.code-gnu-global
+code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.Go
+code --install-extension ms-vscode.powershell
+code --install-extension redhat.java
+code --install-extension redhat.vscode-yaml
+
+
+
+
+
+
+
+
