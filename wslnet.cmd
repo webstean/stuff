9bdad4a6629e24cb8250a9a8f2822350e0aef614
Andrew Webster <webstean@gmail.com>
2020-08-03 20:44:03 +1000

    aAaAA

--- wslnet.cmd
+++ wslnet.cmd
@@ -1,7 +1,12 @@
 @echo off
 
-netsh interface portproxy add v4tov4 listenport=3000 listenaddress=0.0.0.0 connectport=3000 connectaddress=172.27.230.57
-netsh interface portproxy add v4tov4 listenport=6600 listenaddress=0.0.0.0 connectport=6600 connectaddress=172.27.230.57
+netsh interface portproxy add v4tov4 listenport=3000 listenaddress=0.0.0.0 connectport=3000 connectaddress=172.19.222.37
+netsh interface portproxy add v4tov4 listenport=6600 listenaddress=0.0.0.0 connectport=6600 connectaddress=172.19.222.37
+netsh interface portproxy add v4tov4 listenport=6600 listenaddress=0.0.0.0 connectport=55555 connectaddress=172.19.222.37
+
+@rem netsh interface portproxy reset
+
+netsh interface portproxy dump
 
 
 
