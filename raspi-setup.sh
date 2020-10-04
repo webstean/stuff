4cb77c6a6c957aff1c4bedbb515f5c22bbf89a94
Andrew Webster <webstean@gmail.com>
2020-10-04 21:00:20 +1100

    xcbxcvxcvxcvcvx

--- raspi-setup.sh
+++ raspi-setup.sh
@@ -4,17 +4,25 @@
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root - exiting" ; exit 1 ; fi
 
 # Check if this is a Raspberry Pi
-if [ (egrep -q "BCM(283(5|6|7)|270(8|9)|2711)" /proc/cpuinfo) -ne 0 ] ; then echo "This is not a Raspiberry Pi - exiting" ; exit 1 ; fi
+if ( ! cat /proc/cpuinfo | grep Model | grep "Raspberry" ) > /dev/null ; then echo "This is not a Raspiberry Pi - exiting" ; exit 1 ; fi
 
-# Allow any user to run sudo
-bash -c "echo '%sudo ALL=(ALL:ALL) NOPASSWD:ALL' | sudo EDITOR='tee -a' visudo"
+# Enable sudo for all users
+if ! (sudo id | grep -q root) ; then 
+    bash -c "echo '%sudo ALL=(ALL:ALL) NOPASSWD:ALL' | sudo EDITOR='tee -a' visudo"
+fi
 
 # Turn off Swapping
 dphys-swapfile swapoff
 dphys-swapfile uninstall
 update-rc.d dphys-swapfile remove
 
+# Unattended Upgrades
+sudo apt-get install unattended-upgrades
+sudo unattended-upgrade -d -v --dry-run
+sudo dpkg-reconfigure --priority=low unattended-upgrades
+
 # Check Power Supply
-apt-get install sysbench
-https://gist.githubusercontent.com/maxme/d5f000c84a4313aa531288c35c3a8887/raw/fc355cd96e5e18e69df06ee4c34f50b7cd9a4a2a/raspberry-power-supply-check.sh
+apt-get install -y sysbench
+wget https://gist.githubusercontent.com/maxme/d5f000c84a4313aa531288c35c3a8887/raw/fc355cd96e5e18e69df06ee4c34f50b7cd9a4a2a/raspberry-power-supply-check.sh
+chmod +x raspberry-power-supply-check.sh
 ./raspberry-power-supply-check.sh
--- somers/setup.sh
+++ somers/setup.sh
@@ -1,16 +1,20 @@
 # setting up Network Streaming
 
 # install
-sudo apt-get install pulseaudio mpd ncmpcpp
+sudo apt-get install -y pulseaudio mpd ncmpcpp
 
 # Disable pulseaudio (user mode), so we can run it in system mode
 sudo systemctl --global disable pulseaudio.service pulseaudio.socket
+
+# Auxillaries
+sudo apt-get install -y pulseaudio-dlna pasystray pavucontrol paman
+
 
 # All users that need access to PulseAudio have to be in the pulse-access group, even root!
 usermod -a -G pulse-access root
 
 # Create mpd and pulseaudio file
-/etc/mpd.conf
+touch /etc/mpd.conf
 
 # Create mpd directories
 
@@ -28,7 +32,6 @@ chown -R mpd:audio /var/log/mpd
 umask 007 && /usr/bin/pulseaudio --system --daemonize --disallow-exit --disallow-module-loading -vvv
 
 umask 007 && /usr/bin/pulseaudio --realtime --no-cpu-limit --system --verbose --daemonize=no --disallow-exit --disallow-module-loading -vvvvvvvvv
-
 
 sudo bash -c 'cat << EOF > /etc/mpd.conf
 bind_to_address "::"
