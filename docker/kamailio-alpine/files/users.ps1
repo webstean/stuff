88768685fdc39520e88cbe86d61d2336cd8d214d
Andrew Webster <webstean@gmail.com>
2021-06-01 12:41:31 +1000

    keepalinves and users powershell

---
+++ docker/kamailio-alpine/files/users.ps1
@@ -0,0 +1,3 @@
+Set-CsUser -Identity "andreww@lscph.onmicrosoft.com" -OnPremLineURI tel:+14255388701;ext=666 -EnterpriseVoiceEnabled $true -HostedVoiceMail $true
+### Set-CsUser -Identity "stacy.quinn@contoso.com" -OnPremLineURI tel:+14255388701;ext=1002 -EnterpriseVoiceEnabled $true -HostedVoiceMail $true
+
