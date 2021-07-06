276e317481fe253c22a57641f1cdca2ac4b77621
Andrew Webster <webstean@gmail.com>
2021-07-06 16:31:27 +1000

    qqaa

--- setup.sh
+++ setup.sh
@@ -329,19 +329,20 @@ sudo ~/./aws/install
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 # Azure Arc Agent -won't work on WSL VM as they dont run systemd
-# cd ~
-# wget https://aka.ms/azcmagent -O ~/Install_linux_azcmagent.sh
-# bash ~/Install_linux_azcmagent.sh
-# azcmagent connect --resource-group "<resourceGroupName>" --tenant-id "<tenantID>" --location "<regionName>" --subscription-id "<subscriptionID>"
-# azcmagent connect --resource-group "LSCPH-RaspberryPi" --tenant-id "<tenantID>" --location "<regionName>" --subscription-id "2d2089b6-d701-49aa-9600-bc2e3796d53a"
-
-azcmagent connect \
-  --service-principal-id "{serviceprincipalAppID}" \
-  --service-principal-secret "{serviceprincipalPassword}" \
-  --resource-group "LSCPH-RaspberryPi" \
-  --tenant-id "fd72f9ff-96b6-4a20-a870-ceaa17d70bc8" \
-  --location "{resourceLocation}" \
-  --subscription-id "2d2089b6-d701-49aa-9600-bc2e3796d53a"
+if [[ ! $(grep Microsoft /proc/version) ]]; then
+    cd ~
+    wget https://aka.ms/azcmagent -O ~/Install_linux_azcmagent.sh
+    bash ~/Install_linux_azcmagent.sh
+#    azcmagent connect --resource-group "<resourceGroupName>" --tenant-id "<tenantID>" --location "<regionName>" --subscription-id "<subscriptionID>"
+#    azcmagent connect --resource-group "LSCPH-RaspberryPi" --tenant-id "<tenantID>" --location "<regionName>" --subscription-id "2d2089b6-d701-49aa-9600-bc2e3796d53a"
+    azcmagent connect \
+        --service-principal-id "{serviceprincipalAppID}" \
+        --service-principal-secret "{serviceprincipalPassword}" \
+        --resource-group "LSCPH-RaspberryPi" \
+        --tenant-id "fd72f9ff-96b6-4a20-a870-ceaa17d70bc8" \
+        --location "{resourceLocation}" \
+        --subscription-id "2d2089b6-d701-49aa-9600-bc2e3796d53a"
+fi
 
 # Install Google Cloud (GCP) CLI
 cd ~ && curl https://sdk.cloud.google.com > install.sh
