8ba137c2788159dd269166c557076d5fbd22a8fc
Andrew Webster <webstean@gmail.com>
2021-09-30 17:23:16 +1000

    vf

--- chocolatey/mma.ps1
+++ chocolatey/mma.ps1
@@ -1,12 +1,12 @@
 
-MMA Agent
-Windows 64-bit agent - https://go.microsoft.com/fwlink/?LinkId=828603
-Windows 32-bit agent - https://go.microsoft.com/fwlink/?LinkId=828604
+# MMA Agent - permanent download links
+# Windows 64-bit agent - https://go.microsoft.com/fwlink/?LinkId=828603
+# Windows 32-bit agent - https://go.microsoft.com/fwlink/?LinkId=828604
 
-"c:\MMA-Agent\MMASetup-AMD64.exe" /C:"setup.exe /qn ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_ID=44059d47-2bc9-45ea-a5ea-fab50d502a6c OPINSIGHTS_WORKSPACE_KEY=TuOq3w1Ag8Y+8uQl+L9a5mHE1u4/5XAAzhHbUTmA4m34l5sLuimLeVWd+3+33UKKKI+686qsl/gM5XIczuoILQ== AcceptEndUserLicenseAgreement=1"
+# "c:\MMA-Agent\MMASetup-AMD64.exe" /C:"setup.exe /qn ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_ID=44059d47-2bc9-45ea-a5ea-fab50d502a6c OPINSIGHTS_WORKSPACE_KEY=TuOq3w1Ag8Y+8uQl+L9a5mHE1u4/5XAAzhHbUTmA4m34l5sLuimLeVWd+3+33UKKKI+686qsl/gM5XIczuoILQ== AcceptEndUserLicenseAgreement=1"
 
-
-https://go.microsoft.com/fwlink/?LinkId=828603
+choco install microsoft-monitoring-agent -y
+choco install wget -y
 
 # install
 sysmon.exe -accepteula -i sysmonconfig-export.xml
@@ -16,9 +16,8 @@ sysmon.exe -c sysmonconfig-export.xml
 
 https://raw.githubusercontent.com/SwiftOnSecurity/sysmon-config/master/sysmonconfig-export.xml
 
-
-$workspaceId = "<Your workspace Id>"
-$workspaceKey = "<Your workspace Key>"
+$workspaceId = "74328598-0b07-4a05-92d8-f0fbb9271c20"
+$workspaceKey = "2ul7XdqK4bCQfAJEg/iNhPbqWoRABHdozUWChMHx3mmMTLSW1i78sQ+WujUNvtmi88MkDcklgT/v5FbzIlAJvQ=="
 $mma = New-Object -ComObject 'AgentConfigManager.MgmtSvcCfg'
 $mma.AddCloudWorkspace($workspaceId, $workspaceKey)
 $mma.ReloadConfiguration()
