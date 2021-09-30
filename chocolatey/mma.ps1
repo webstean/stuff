d856eaf45a09fcd23a62d3d7084e9f056b283891
Andrew Webster <webstean@gmail.com>
2021-09-30 17:40:11 +1000

    sent

--- chocolatey/mma.ps1
+++ chocolatey/mma.ps1
@@ -6,18 +6,16 @@
 # "c:\MMA-Agent\MMASetup-AMD64.exe" /C:"setup.exe /qn ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_ID=44059d47-2bc9-45ea-a5ea-fab50d502a6c OPINSIGHTS_WORKSPACE_KEY=TuOq3w1Ag8Y+8uQl+L9a5mHE1u4/5XAAzhHbUTmA4m34l5sLuimLeVWd+3+33UKKKI+686qsl/gM5XIczuoILQ== AcceptEndUserLicenseAgreement=1"
 
 choco install microsoft-monitoring-agent -y
-choco install wget -y
-
-# install
-sysmon.exe -accepteula -i sysmonconfig-export.xml
-
-# update 
-sysmon.exe -c sysmonconfig-export.xml
-
-https://raw.githubusercontent.com/SwiftOnSecurity/sysmon-config/master/sysmonconfig-export.xml
-
 $workspaceId = "74328598-0b07-4a05-92d8-f0fbb9271c20"
 $workspaceKey = "2ul7XdqK4bCQfAJEg/iNhPbqWoRABHdozUWChMHx3mmMTLSW1i78sQ+WujUNvtmi88MkDcklgT/v5FbzIlAJvQ=="
 $mma = New-Object -ComObject 'AgentConfigManager.MgmtSvcCfg'
 $mma.AddCloudWorkspace($workspaceId, $workspaceKey)
 $mma.ReloadConfiguration()
+
+# install sysmon
+choco install wget -y
+wget https://raw.githubusercontent.com/SwiftOnSecurity/sysmon-config/master/sysmonconfig-export.xml -o 'C:\Program Files\Microsoft Monitoring Agent\sysmonconfig-export.xml'
+sysmon.exe -accepteula -i 'C:\Program Files\Microsoft Monitoring Agent\sysmonconfig-export.xml'
+
+# update sysmon 
+sysmon.exe -c 'C:\Program Files\Microsoft Monitoring Agent\sysmonconfig-export.xml'
