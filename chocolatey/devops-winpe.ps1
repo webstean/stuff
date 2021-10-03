d9a94d56d6462ebd2e117db749bcb30311e7c243
Andrew Webster <webstean@gmail.com>
2021-10-04 01:15:23 +1100

    aaaaa

--- chocolatey/devops-winpe.ps1
+++ chocolatey/devops-winpe.ps1
@@ -2,3 +2,15 @@
 choco install windows-adk -y 
 choco install windows-adk-winpe -y 
 
+List disk
+select disk X    (where X is your USB drive)
+clean
+create partition primary size=2048
+active
+format fs=FAT32 quick label="WinPE"
+assign letter=P
+create partition primary
+format fs=NTFS quick label="Images"
+assign letter=I  
+Exit
+
