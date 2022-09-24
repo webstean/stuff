0b05cb3c65b14b67ed13f51c101dee31660da868
Andrew Webster <webstean@gmail.com>
2022-09-25 08:44:28 +1000

    Update wsl.cmd

--- wsl.cmd
+++ wsl.cmd
@@ -1,4 +1,10 @@
 @echo off
 
+wsl --install
 wsl --set-default-version 2
-winget install --id Canonical.Ubuntu
+wsl --update
+wsl --install Ubuntu-20.04 
+wsl --setdefault Ubuntu-20.04 
+wsl --status
+
+@rem winget install --id Canonical.Ubuntu
