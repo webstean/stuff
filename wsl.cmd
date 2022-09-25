2f3d8189529500c61fbe7420516d6459d646f5fc
Andrew Webster <webstean@gmail.com>
2022-09-25 16:17:50 +1000

    Update wsl.cmd

--- wsl.cmd
+++ wsl.cmd
@@ -6,6 +6,7 @@ wsl --update
 wsl --install Ubuntu-20.04 
 wsl --setdefault Ubuntu-20.04 
 wsl --status
+%USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe config --default-user root
 
 @rem winget install --id Canonical.Ubuntu
 @rem winget uninstall --id Canonical.Ubuntu
