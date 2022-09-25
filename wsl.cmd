228ada8e0059f178639166d55aa16a8185ffb088
Andrew Webster <webstean@gmail.com>
2022-09-25 16:49:59 +1000

    sasas

--- wsl.cmd
+++ wsl.cmd
@@ -7,6 +7,12 @@ wsl --install Ubuntu-20.04
 wsl --setdefault Ubuntu-20.04 
 wsl --status
 %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ubuntu2004.exe config --default-user root
+cd %SYSTEMROOT%\System32\lxss\lib
+del libcuda.so
+mklink libcuda.so libcuda.so.1
+
+in WSL 
+sudo ldconfig
 
 @rem winget install --id Canonical.Ubuntu
 @rem winget uninstall --id Canonical.Ubuntu
